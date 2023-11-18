6d687db88d85d9b6a2bc29fd90d9f830df3e60c4
msommerfe <marco.sommerfeld@t-online.de>
2023-11-18 12:45:39 +0100

    Datens für training vorbereitet

--- Module 3 - Data Analysis and Machine Learning/project/descriptive_statistic_publiBike_availability.py
+++ Module 3 - Data Analysis and Machine Learning/project/descriptive_statistic_publiBike_availability.py
@@ -7,23 +7,70 @@ from IPython.display import display
 pd.set_option('display.max_columns', None)
 pd.set_option('expand_frame_repr', False)
 
-import io
-import matplotlib.dates as mdates
 
 dfPubliBikeAvailability = pd.read_csv("data/publi-e-bike-availability-bern.csv", encoding='latin-1', sep=';')
+
 dfPubliBikeAvailability = dfPubliBikeAvailability.rename(columns={"filename": "timestamp"})
+dfPubliBikeAvailability["timestamp"] = pd.to_datetime(dfPubliBikeAvailability["timestamp"])
+dfPubliBikeAvailability["dayofweek"] = dfPubliBikeAvailability["timestamp"].dt.weekday
+dfPubliBikeAvailability["hourofday"] = dfPubliBikeAvailability["timestamp"].dt.hour
+dfPubliBikeAvailability["minuteofday"] = dfPubliBikeAvailability["timestamp"].dt.minute
+dfPubliBikeAvailability['station_id'] = dfPubliBikeAvailability['stationsname'].astype('category').cat.codes
 print(dfPubliBikeAvailability)
-dfPubliBikeAvailability = dfPubliBikeAvailability.sort_values(by='timestamp', ascending=True)
-
-dfBreitenrainstrasse = dfPubliBikeAvailability[dfPubliBikeAvailability["stationsname"] == "Breitenrainstrasse"]
-dfBreitenrainstrasse["timestamp"] = pd.to_datetime(dfBreitenrainstrasse["timestamp"])
-print(dfBreitenrainstrasse)
 
 
-fig = px.line(y=dfBreitenrainstrasse["anzahl_e_bikes"], x=dfBreitenrainstrasse["timestamp"])
-fig.show()
+
+#############################################################################################
+######## Analyse Data for just one Station. In this case station Breitenrainstrasse##########
+#############################################################################################
+
+#dfPubliBikeAvailability = dfPubliBikeAvailability.sort_values(by='timestamp', ascending=True)
+#dfBreitenrainstrasse = dfPubliBikeAvailability[dfPubliBikeAvailability["stationsname"] == "Breitenrainstrasse"]
+#fig = px.line(y=dfBreitenrainstrasse["anzahl_e_bikes"], x=dfBreitenrainstrasse["timestamp"])
+#fig.show()
+#print(dfBreitenrainstrasse)
 
 
-# Cluster Ansatz. Gruppen sind die Anzahl Bikes und input ist TimeStamp + Location Name
 
 
+
+#############################################################################################
+######  Preparing Data fpr Training. Split in Train and Test Data  ##########################
+######  Feature: [dayofweek, hour, Min, Stationname] Label:  anzahl_e_bikes     ##########################
+#############################################################################################
+
+# percentage to split the data into train and test data
+percentTrain = 0.8
+rangeTrain= int(np.round(dfPubliBikeAvailability.shape[0] * percentTrain))
+
+#split data tran and test
+dfFeatureTrainPB = dfPubliBikeAvailability[['station_id','dayofweek','hourofday','minuteofday']][:rangeTrain]
+dfLabelsTrainPB = dfPubliBikeAvailability[['anzahl_e_bikes']][:rangeTrain]
+
+dfFeaturesTestPB =  dfPubliBikeAvailability[['station_id','dayofweek','hourofday','minuteofday']][rangeTrain:]
+dfLabelsTestPB =  dfPubliBikeAvailability[['anzahl_e_bikes']][rangeTrain:]
+print(dfFeatureTrainPB.shape)
+print(type(dfFeatureTrainPB))
+
+
+
+dfFeatureTrainPB = dfFeatureTrainPB.to_numpy()
+dfLabelsTrainPB = dfLabelsTrainPB.to_numpy()
+
+dfFeaturesTestPB = dfFeaturesTestPB.to_numpy()
+dfLabelsTestPB = dfLabelsTestPB.to_numpy()
+print("klhgluhl")
+print(dfLabelsTrainPB.shape)
+print(type(dfLabelsTrainPB))
+
+
+print("rangeTrain: ", rangeTrain)
+print("sizePB: ", dfPubliBikeAvailability.shape)
+print("sizeTrain: ", dfFeatureTrainPB.shape)
+print("sizeTest: ", dfFeaturesTestPB.shape)
+#print(dfPubliBikeAvailability.size)
+print(dfFeatureTrainPB)
+print(dfLabelsTrainPB)
+
+
+#### Cluster Ansatz. Gruppen sind die Anzahl Bikes und input ist TimeStamp + Location Name #######
